fb4c3ceaa44ba11bd81688ce71b1821a8def0565
Diaz de Guereñu Serrano <diazdeguerenu.danel@uni.lan>
2020-09-29 14:09:50 +0200

    toma

--- ej2.php
+++ ej2.php
@@ -5,16 +5,21 @@ $a = 5;
 $b = 8;
 
 if($a==5) {
-    print "<p> a igual que 5.  </p>"
+    print "<p> a igual que 5.</p>";
 }
+
 elseif ($a<7) {
-    print"<p>A es menor 7"
+    print"<p>A es menor 7</p>";
 }
 
 if($a>5 && $a<7) {
-    print "<p> a es menor que 7 y mayor que 5.  </p>"
+    print "<p> a es menor que 7 y mayor que 5.</p>";
 }
 else {
-print "niidea primo"
+print "<p>niidea primo</p>";
+}
+
+for ($contador =0; $contador <6; $contador++) {
+    print"img src=\"img/dado-1/&contador"
 }
 ?>
